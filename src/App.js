4de2bb96e3f6dc8a85455a9191e16a457b71855b
chestergarett <chestergarett@gmail.com>
2021-07-07 19:32:31 +0800

    adding login, font-family components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Login from './components/Login/Login';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
     
+    <Login />
+    <Header />
     </>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import "./Header.css";
+import MenuIcon from '@material-ui/icons/Menu';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import HistoryIcon from '@material-ui/icons/History';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SearchIcon from '@material-ui/icons/Search';
+import logo from "../../logo.svg";
+
+
+const Header = () => {
+
+
+    return (
+        <div className="header__container">
+            <div className="header">
+                <div className="header__menu"><MenuIcon /></div>
+                <div className="header__history">
+                    <ArrowBackIcon />
+                    <ArrowForwardIcon />
+                    <HistoryIcon />
+                    <div className="header__search">
+                        <span>Search</span>
+                        <SearchIcon />
+                    </div>
+                    <HelpOutlineIcon />
+                </div>
+                <div className="header__user-info">
+                    <img src={logo} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
