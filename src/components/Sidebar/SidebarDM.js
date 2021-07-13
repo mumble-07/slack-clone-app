4183ea74a61fc651dbd2b8c3b39fc7d99664f167
Andy <andreapauline.deguzman@gmail.com>
2021-07-13 20:49:35 +0800

    andy: adding retrieve message, refactor Chatbox and separated Chatbox receive and chatbox send into two files. Created ChatBoxMain file

--- src/components/Sidebar/SidebarDM.js
+++ src/components/Sidebar/SidebarDM.js
@@ -1,33 +1,43 @@
-import { ContactlessOutlined } from '@material-ui/icons';
-import {useContext} from 'react';
-import UserContext from '../../api/user-context.js';
+import { ContactlessOutlined } from "@material-ui/icons";
+import { useContext } from "react";
+import UserContext from "../../api/user-context.js";
 
 const SidebarDM = () => {
+  const { rawUserList, chatScreenData } = useContext(UserContext);
 
-    const {rawUserList, chatScreenData} = useContext(UserContext);
+  const showChatScreen = (e) => {
+    chatScreenData.type = e.currentTarget.type;
+    chatScreenData.receivers = [
+      {
+        id: e.currentTarget.id,
+        name: e.currentTarget.getAttribute("name"),
+        type: e.currentTarget.type,
+      },
+    ];
+    console.log(chatScreenData);
+    // console.log()
+  };
 
-    const showChatScreen = (e) => {
-        chatScreenData.type = e.currentTarget.type;
-        chatScreenData.receivers = [{ 
-            id: e.currentTarget.id,
-            name: e.currentTarget.getAttribute("name"),
-            type: e.currentTarget.type,
-        }]
-        console.log(chatScreenData)
-        // console.log()
-    }
-
-    return (
-        <>
-        {rawUserList[0].map(user => {
-            const userName = user.name ? user.name : user.email
-            return (
-                <li key={user.id} id={user.id} name={userName} type="User" onClick={showChatScreen} ><a href="/#"><box-icon name='user-circle' ></box-icon> {user.email} </a></li>
-            )
-        })
-        }    
-        </>
-    )
-}
+  return (
+    <>
+      {rawUserList[0].map((user) => {
+        const userName = user.name ? user.name : user.email;
+        return (
+          <li
+            key={user.id}
+            id={user.id}
+            name={userName}
+            type="User"
+            onClick={showChatScreen}
+          >
+            <a href="/#">
+              <box-icon name="user-circle"></box-icon> {user.email}{" "}
+            </a>
+          </li>
+        );
+      })}
+    </>
+  );
+};
 
 export default SidebarDM;
