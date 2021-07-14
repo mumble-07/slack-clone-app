491718275d7c044222b3262efa4a22b5289b294c
Andy <andreapauline.deguzman@gmail.com>
2021-07-14 21:00:34 +0800

    andy: added Users search bar for sideBarDM

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,130 +1,147 @@
-import {useState, useContext,useEffect} from "react";
-import UserContext from '../../api/user-context.js';
-import axios from 'axios';
+import { useState, useContext, useEffect } from "react";
+import UserContext from "../../api/user-context.js";
+import axios from "axios";
 import "./Sidebar.css";
-import 'boxicons';
+import "boxicons";
 
-import SidebarChannel from './SidebarChannel';
-import SidebarDM from './SidebarDM';
-
+import SidebarChannel from "./SidebarChannel";
+import SidebarDM from "./SidebarDM";
 
 const Sidebar = () => {
-    const {chatScreenData} = useContext(UserContext) //modify current receiver when EDIT button is clicked
-    const [toggleChannel, setToggleChannel] = useState(false)
-    const [toggleDM, setToggleDM] = useState(false)
+  const { chatScreenData } = useContext(UserContext); //modify current receiver when EDIT button is clicked
+  const [toggleChannel, setToggleChannel] = useState(false);
+  const [toggleDM, setToggleDM] = useState(false);
 
-    const toggleChannelHandler = () => {
-        setToggleChannel(prevValue => !prevValue)
+  const toggleChannelHandler = () => {
+    setToggleChannel((prevValue) => !prevValue);
+  };
+
+  const toggleDMHandler = () => {
+    setToggleDM((prevValue) => !prevValue);
+  };
+
+  const { userListHeaders, channelList, rawUserList, setUpHeaders } =
+    useContext(UserContext);
+
+  useEffect(() => {
+    const storage = localStorage.getItem("user");
+    if (storage) {
+      const {
+        "access-token": accessToken,
+        client,
+        expiry,
+        uid,
+      } = JSON.parse(storage);
+      setUpHeaders(accessToken, client, expiry, uid);
     }
+    axios
+      .get("http://206.189.91.54//api/v1/channels", {
+        headers: userListHeaders,
+      })
+      .then((res) => {
+        const { data } = res;
+        if ((data.errors = "No available channels")) {
+          console.log(data.errors);
+          return;
+        }
+        channelList.push(data);
+        console.log(channelList);
+      })
+      .catch((error) => console.error("Error fetching data from API"));
+  }, []);
 
-    const toggleDMHandler = () => {
-        setToggleDM(prevValue => !prevValue)
+  useEffect(() => {
+    const storage = localStorage.getItem("user");
+    if (storage) {
+      const {
+        "access-token": accessToken,
+        client,
+        expiry,
+        uid,
+      } = JSON.parse(storage);
+      setUpHeaders(accessToken, client, expiry, uid);
     }
-
-    const {userListHeaders,channelList, rawUserList, setUpHeaders} = useContext(UserContext);
-
-    useEffect( () => {
-        const storage = localStorage.getItem('user');
-        if(storage){
-            const {"access-token": accessToken, client, expiry, uid} =  JSON.parse(storage);
-            setUpHeaders(accessToken,client,expiry,uid);
+    axios
+      .get("http://206.189.91.54//api/v1/users", { headers: userListHeaders })
+      .then((res) => {
+        const { data } = res;
+        if (data.length == 0) {
+          console.log("No available users");
+          return;
         }
-        axios.get('http://206.189.91.54//api/v1/channels', {headers: userListHeaders})
-        .then(res => {
-            const {data} = res;
-            if(data.errors = "No available channels"){ 
-                console.log(data.errors);
-                return 
-            }
-            channelList.push(data)
-            console.log(channelList)
-        })
-        .catch(error => console.error('Error fetching data from API'))
-        }
-        , []
-    )
+        rawUserList.push(data.data);
+        console.log(rawUserList);
+      })
+      .catch((error) => console.error("Error fetching data from API"));
+  }, []);
 
-    useEffect( () => {
-        const storage = localStorage.getItem('user');
-        if(storage){
-            const {"access-token": accessToken, client, expiry, uid} =  JSON.parse(storage);
-            setUpHeaders(accessToken,client,expiry,uid);
-        }
-        axios.get('http://206.189.91.54//api/v1/users', {headers: userListHeaders})
-        .then(res => {
-            const {data} = res;
-            if(data.length == 0){ 
-                console.log("No available users");
-                return 
-            }
-            rawUserList.push(data.data)
-            console.log(rawUserList)
-        })
-        .catch(error => console.error('Error fetching data from API'))
-        }
-        , []
-    )
+  const setChatScreenData = () => {
+    chatScreenData["type"] = "new";
+    chatScreenData["receivers"] = [];
+  };
 
-    const setChatScreenData = () => {
-        chatScreenData["type"] = "new"
-        chatScreenData["receivers"] = [];
-    }
-
-    return (
-        <div className="wrapper">
-        <div className="sidebar">
-            <div className="channel-details">
-                <div className="channel_name">Group 5</div>
-                <box-icon name='edit' onClick={setChatScreenData}></box-icon>
+  return (
+    <div className="wrapper">
+      <div className="sidebar">
+        <div className="channel-details">
+          <div className="channel_name">Group 5</div>
+          <box-icon name="edit" onClick={setChatScreenData}></box-icon>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <box-icon name="message-alt-detail"></box-icon>
+            <span className="links_name">Threads</span>
+          </li>
+          <li>
+            <box-icon name="conversation"></box-icon>
+            <span className="links_name">All DMs</span>
+          </li>
+          <li>
+            <box-icon name="file-blank"></box-icon>
+            <span className="links_name">Drafts</span>
+          </li>
+          <li>
+            <box-icon name="message-alt-error"></box-icon>
+            <span className="links_name">Mention & reactions</span>
+          </li>
+          <li>
+            <div className="icon-link">
+              <div className="channelsHandler" onClick={toggleChannelHandler}>
+                {!toggleChannel && (
+                  <box-icon name="chevron-right-circle"></box-icon>
+                )}
+                {toggleChannel && (
+                  <box-icon name="chevron-down-circle"></box-icon>
+                )}
+                <span className="links_name">Channels</span>
+              </div>
             </div>
-            <ul className="nav-links">
-                <li>
-                        <box-icon name='message-alt-detail' ></box-icon>
-                        <span className="links_name">Threads</span>
-                </li>
-                <li>
-                        <box-icon name='conversation' ></box-icon>
-                        <span className="links_name">All DMs</span>
-                </li>
-                <li>
-                        <box-icon name='file-blank' ></box-icon>
-                        <span className="links_name">Drafts</span>
-                </li>
-                <li>
-                        <box-icon name='message-alt-error' ></box-icon>
-                        <span className="links_name">Mention & reactions</span>
-                </li>
-                <li>
-                    <div className="icon-link">
-                    <div className="channelsHandler" onClick={toggleChannelHandler}> 
-                        {!toggleChannel && <box-icon name='chevron-right-circle' ></box-icon>}
-                        {toggleChannel && <box-icon name ='chevron-down-circle'></box-icon>}
-                        <span className="links_name">Channels</span>
-                    </div>
-                    </div>
-                    <ul className="sub-menu">
-                        <li><box-icon name='lock-alt' ></box-icon>batch 9</li>
-                        {toggleChannel && <SidebarChannel/>}
-                        <li><box-icon name='lock-alt' ></box-icon>Add channels</li>
-                    </ul>
-                </li>
-                <li>
-                    <div className="icon-link">
-                        <div className="dmHandler" onClick={toggleDMHandler}>
-                            {!toggleDM && <box-icon name='chevron-right-circle' ></box-icon>}
-                            {toggleDM && <box-icon name='chevron-down-circle' ></box-icon>}
-                            <span className="links_name">Direct Messages</span>
-                        </div>
-                    </div>
-                    <ul className="sub-menu dm-list">
-                        {toggleDM && <SidebarDM className="sidebarDM"/>}
-                    </ul>
-                </li>
+            <ul className="sub-menu">
+              <li>
+                <box-icon name="lock-alt"></box-icon>batch 9
+              </li>
+              {toggleChannel && <SidebarChannel />}
+              <li>
+                <box-icon name="lock-alt"></box-icon>Add channels
+              </li>
             </ul>
-        </div>
-        </div>
-    )
-}
+          </li>
+          <li>
+            <div className="icon-link">
+              <div className="dmHandler" onClick={toggleDMHandler}>
+                {!toggleDM && <box-icon name="chevron-right-circle"></box-icon>}
+                {toggleDM && <box-icon name="chevron-down-circle"></box-icon>}
+                <span className="links_name">Direct Messages</span>
+              </div>
+            </div>
+            <ul className="sub-menu dm-list">
+              {toggleDM && <SidebarDM className="sidebarDM" />}
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
-  
--- src/components/Sidebar/SidebarDM.js
+++ src/components/Sidebar/SidebarDM.js
@@ -1,63 +1,100 @@
 import { ContactlessOutlined } from "@material-ui/icons";
 import { useContext } from "react";
-import axios from 'axios';
+import axios from "axios";
 import UserContext from "../../api/user-context.js";
+import { useState } from "react";
 
 const SidebarDM = () => {
-  
-  const { 
+  const {
     rawUserList,
-    userDetails, 
-    userListHeaders, 
-    chatScreenData, 
-    retrieveMessageAccountOwnerReceiver, 
-    retrieveMessageAccountOwnerSender, 
-    setUpretrieveMessageAccountOwnerReceiver, 
+    userDetails,
+    userListHeaders,
+    chatScreenData,
+    retrieveMessageAccountOwnerReceiver,
+    retrieveMessageAccountOwnerSender,
+    setUpretrieveMessageAccountOwnerReceiver,
     setUpretrieveMessageAccountOwnerSender,
-    currentMessage } =  useContext(UserContext);
+    currentMessage,
+  } = useContext(UserContext);
 
-    const showChatScreen = (e) => {
-        chatScreenData.type = e.currentTarget.type;
-        chatScreenData.receivers = [{ 
-            id: e.currentTarget.id,
-            name: e.currentTarget.getAttribute("name"),
-            type: e.currentTarget.type,
-        }]
-        setUpretrieveMessageAccountOwnerReceiver(Number(chatScreenData.receivers[0].id),chatScreenData.receivers[0].type,Number(userDetails[0].id));
-        setUpretrieveMessageAccountOwnerSender(Number(userDetails[0].id),chatScreenData.receivers[0].type,Number(chatScreenData.receivers[0].id));
-        //retrieve message as receiver
-        axios.get("http://206.189.91.54//api/v1/messages", {
-        headers: userListHeaders, params: retrieveMessageAccountOwnerReceiver,
-        })
-        .then((response) => response)
-        .then((result) => currentMessage.push(result))
-        .catch((error) => error)
+  const showChatScreen = (e) => {
+    chatScreenData.type = e.currentTarget.type;
+    chatScreenData.receivers = [
+      {
+        id: e.currentTarget.id,
+        name: e.currentTarget.getAttribute("name"),
+        type: e.currentTarget.type,
+      },
+    ];
+    setUpretrieveMessageAccountOwnerReceiver(
+      Number(chatScreenData.receivers[0].id),
+      chatScreenData.receivers[0].type,
+      Number(userDetails[0].id)
+    );
+    setUpretrieveMessageAccountOwnerSender(
+      Number(userDetails[0].id),
+      chatScreenData.receivers[0].type,
+      Number(chatScreenData.receivers[0].id)
+    );
+    //retrieve message as receiver
+    axios
+      .get("http://206.189.91.54//api/v1/messages", {
+        headers: userListHeaders,
+        params: retrieveMessageAccountOwnerReceiver,
+      })
+      .then((response) => response)
+      .then((result) => currentMessage.push(result))
+      .catch((error) => error);
 
-        //retrieve message as sender
-        axios.get("http://206.189.91.54//api/v1/messages", {
-        headers: userListHeaders, params: retrieveMessageAccountOwnerSender
-          })
-        .then((response) => response)
-        .then((result) => currentMessage.push(result))
-        .catch((error) => error)
-        }
-    
+    //retrieve message as sender
+    axios
+      .get("http://206.189.91.54//api/v1/messages", {
+        headers: userListHeaders,
+        params: retrieveMessageAccountOwnerSender,
+      })
+      .then((response) => response)
+      .then((result) => currentMessage.push(result))
+      .catch((error) => error);
+  };
+
+  //Searh Bar
+
+  const [searchUser, setSearchUser] = useState("");
+
   return (
     <>
-      {rawUserList[0].map((user) => {
-        const userName = user.name ? user.name : user.email;
-        return (
-          <li
-            key={user.id}
-            id={user.id}
-            name={userName}
-            type="User"
-            onClick={showChatScreen}
-          >
+      <input
+        style={{ width: "100%" }}
+        type="text"
+        placeholder="search user"
+        onChange={(event) => {
+          setSearchUser(event.target.value);
+        }}
+      />
+      {rawUserList[0]
+        .filter((value) => {
+          if (searchUser === "") {
+            return value;
+          } else if (
+            value.uid.toLowerCase().includes(searchUser.toLowerCase())
+          ) {
+            return value;
+          }
+        })
+        .map((user) => {
+          const userName = user.name ? user.name : user.email;
+          return (
+            <li
+              key={user.id}
+              id={user.id}
+              name={userName}
+              type="User"
+              onClick={showChatScreen}
+            >
               <box-icon name="user-circle"></box-icon> {user.email}{" "}
-          </li>
-        );
-      })}
+            </li>
+          );
+        })}
     </>
   );
 };
