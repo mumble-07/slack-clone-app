e6790330824dd5f72478b53c8c526035fe08c3ba
Brando <brandomoreno06@outlook.com>
2021-07-15 19:49:18 +0800

    fixed-bug in retrive message

--- src/components/ChatScreen/ChatsRetrieved.js
+++ src/components/ChatScreen/ChatsRetrieved.js
@@ -14,21 +14,23 @@ const ChatsRetrieved = () => {
 
     //retrieve messages API call
     const retrieveMessage = () => {
-        axios.get("http://206.189.91.54//api/v1/messages", {
-        headers: userListHeaders, 
-        params:  {
-        "sender_id": userDetails[0].id.toString(),
-        "receiver_class": "User",
-        "receiver_id": chatScreenData.receivers[0].id,
-        },})
-        .then((response) => response.data.data)
-        .then((result) => {
-        currentMessage[0]= result;
-        console.log(currentMessage[0]);
-        setMessages(currentMessage[0]);
-        setIsLoading(false);
-        })
-        .catch((error) => error)
+        if(chatScreenData.receivers.length !== 0) {
+            axios.get("http://206.189.91.54//api/v1/messages", {
+            headers: userListHeaders, 
+            params:  {
+            "sender_id": userDetails[0].id.toString(),
+            "receiver_class": "User",
+            "receiver_id": chatScreenData.receivers[0].id,
+            },})
+            .then((response) => response.data.data)
+            .then((result) => {
+            currentMessage[0]= result;
+            console.log(currentMessage[0]);
+            setMessages(currentMessage[0]);
+            setIsLoading(false);
+            })
+            .catch((error) => error)
+        } else return;
     }
 
 
@@ -48,34 +50,39 @@ const ChatsRetrieved = () => {
     }, [])
 
 
-    
+
     return(
         <div>
-            {isLoading && <div>Loading messages...</div>}
-            {!isLoading &&
-                <ul className="chat-container">
-                    {messages?.map(message => {
-                        const messageContent = message.body;
-                        const name = message.sender.name ? message.sender.name :  message.sender.email;
-                        const time = new Date(message.created_at).toLocaleTimeString();
-                        return (
-                            <li key={message.id} className="chat-item-container">
-                                <div className="chat__user-image">
-                                    <img src={user1} alt="sender image" />
-                                </div>
-                                <div className="chat-item">
-                                    <div className="chat-item__header">
-                                        <h5>{name}</h5>
-                                        <span className="chat-time">{time}</span>
+            {isLoading && <div className="no-message__alert"><h5>Loading...</h5></div>}
+
+            {!isLoading && 
+                (messages?.length > 0 ?
+                    <ul className="chat-container">
+                        {messages?.map(message => {
+                            const messageContent = message.body;
+                            const name = message.sender.name ? message.sender.name :  message.sender.email;
+                            const time = new Date(message.created_at).toLocaleTimeString();
+                            return (
+                                <li key={message.id} className="chat-item-container">
+                                    <div className="chat__user-image">
+                                        <img src={user1} alt="sender image" />
                                     </div>
-                                    <div className="chat__content">
-                                        {parse(messageContent)}
+                                    <div className="chat-item">
+                                        <div className="chat-item__header">
+                                            <h5>{name}</h5>
+                                            <span className="chat-time">{time}</span>
+                                        </div>
+                                        <div className="chat__content">
+                                            {parse(messageContent)}
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        )
-                    })}
-                </ul>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    :
+                    <div className="no-message__alert"><h5>No available message.</h5></div>
+                )
             }
         </div>
     )
